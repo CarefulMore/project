3d404fde72ff41262d4d835aa8136dcbbc027bee
CarefulMore <50913837+CarefulMore@users.noreply.github.com>
2022-01-11 15:01:32 +0300

    Change js

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
-/*const slides = document.querySelectorAll ('.slide')
+const slides = document.querySelectorAll ('.slide')
 
 for (const slide of slides) {
     slide.addEventListener ('click', () => {
         clearActiveClasses ()
         slide.classList.add ('active')
     })
-}*/
+}
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName('.slide')
-
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none'; 
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = 'block'; 
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-  
-  }
-
-
-/*function clearActiveClasses () {
+function clearActiveClasses () {
     slides.forEach ((slide) => {
         slide.classList.remove ('active')
     })
-}*/
+}
 
 
